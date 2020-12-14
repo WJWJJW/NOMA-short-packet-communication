c0e395931b9f157d01e64fd2bc9d39d4ba33c7f3
wj wang <eric79680227@gmail.com>
2020-12-14 18:48:16 +0800

    feat: method of random pick modification, return change pair only

--- User Pairing/neighbor_finder.m
+++ User Pairing/neighbor_finder.m
@@ -1,11 +1,12 @@
-function [neighbor_1, neighbor_2] = neighbor_finder(cur_combinition, K)
+function [neighbor_1, neighbor_2, pair_idx] = neighbor_finder(cur_combinition, K)
     
-    while 1
-        pair_idx = randi(K,1,2);
-        if pair_idx(1) ~= pair_idx(2)
-            break;
-        end
-    end
+    % random pick
+    pair_idx = zeros(1,2,'uint32');
+    pair_idx(1) = randi(K,1);
+    valid_choices = setdiff (1:K, pair_idx(1));
+    pair_idx(2) = valid_choices(randi(K-1));
+
+    
     % neighbor 1
     pairA = cur_combinition(pair_idx(1),:);
     pairB = cur_combinition(pair_idx(2),:);
@@ -16,10 +17,11 @@ function [neighbor_1, neighbor_2] = neighbor_finder(cur_combinition, K)
     % sort again
     pairA = sort(pairA);
     pairB = sort(pairB);
-    % output
-    neighbor_1 = cur_combinition;
-    neighbor_1(pair_idx(1),:) = pairA;
-    neighbor_1(pair_idx(2),:) = pairB;
+%     % output
+%     neighbor_1 = cur_combinition;
+%     neighbor_1(pair_idx(1),:) = pairA;
+%     neighbor_1(pair_idx(2),:) = pairB;
+    neighbor_1 = [pairA; pairB];
     
     % neighbor 2
     pairA = cur_combinition(pair_idx(1),:);
@@ -31,8 +33,9 @@ function [neighbor_1, neighbor_2] = neighbor_finder(cur_combinition, K)
     % sort again
     pairA = sort(pairA);
     pairB = sort(pairB);
-    % output
-    neighbor_2 = cur_combinition;
-    neighbor_2(pair_idx(1),:) = pairA;
-    neighbor_2(pair_idx(2),:) = pairB;
+%     % output
+%     neighbor_2 = cur_combinition;
+%     neighbor_2(pair_idx(1),:) = pairA;
+%     neighbor_2(pair_idx(2),:) = pairB;
+    neighbor_2 = [pairA; pairB];
 end
