3e89d21dbd2392c4abc47ba0179115366be12c8d
wj wang <eric79680227@gmail.com>
2020-12-14 18:49:25 +0800

    feat: Funciton call modification

--- User Pairing/NOMA_HARQ_IR_short_packet_BL_UP_1times.m
+++ User Pairing/NOMA_HARQ_IR_short_packet_BL_UP_1times.m
@@ -24,22 +24,36 @@ dis_thred1 = (eplsion2R/eplsion1R)^(1/eta);
 user_distance = randi([10 330],1,2*K);
 user_distance = sort(user_distance);
 
-RP_user_pairing = zeros(K,2);
-User_pre_grouping = zeros(K,2);
 Timer = zeros(2, length(Pt));
 
+RP_user_pairing = zeros(K,2,length(Pt));
+User_pre_grouping = zeros(K,2,length(Pt));
+Simulated_Anealing_Pairing = zeros(K,2,length(Pt));
+
+sum_RP_opt_M = zeros(1,length(Pt));
+sum_UPG_opt_M = zeros(1,length(Pt));
+sum_HCP_opt_M = zeros(1,length(Pt));
+sum_SAP_opt_M = zeros(1,length(Pt));
+
+HCP_opt_M = zeros(K, length(Pt));
+SAP_opt_M = zeros(K, length(Pt));
+
+
 for u=1:length(Pt)
+
+    h = (randn(1,N)+1i*randn(1,N));
+    lamda = mean(abs(h).^2);
     %% Random Paring (RP)
     RP_indices = randperm(2*K);
     for ii=1:K
         RP_user_pairing(ii,:,u) = sort(user_distance(RP_indices(2*ii-1:2*ii)));
     end
     
-    [RP_thred1_check(:,u), RP_thred2_check(:,u)] = thred_checker(RP_user_pairing(:,:,u), K, eplsion1R, eplsion2R, rho(u), eta);
+%     [RP_thred1_check(:,u), RP_thred2_check(:,u)] = thred_checker(RP_user_pairing(:,:,u), K, eplsion1R, eplsion2R, rho(u), eta);
     
     % Total blocklength for random pairing
-    [sum_RP_opt_M(u), RP_opt_M(:,u)] = M_cal(N1, RP_user_pairing(:,:,u), K,...
-                                        eplsion1R,eplsion2R,rho(u),N,eta);
+    [sum_RP_opt_M(u), RP_opt_M] = M_cal(N1, RP_user_pairing(:,:,u), K,...
+                                        eplsion1R,eplsion2R,rho(u),eta,lamda);
     
     %% User Pre-Grouping
     for ii=1:K
@@ -47,35 +61,44 @@ for u=1:length(Pt)
         User_pre_grouping(ii,2,u) = user_distance(K -1 + ii);
     end
     
-    [UPG_thred1_check(:,u), UPG_thred2_check(:,u)] = thred_checker(User_pre_grouping(:,:,u), K, eplsion1R, eplsion2R, rho(u), eta);
+%     [UPG_thred1_check(:,u), UPG_thred2_check(:,u)] = thred_checker(User_pre_grouping(:,:,u), K, eplsion1R, eplsion2R, rho(u), eta);
     
     % Total blocklength for User Pre-Grouping
-    [sum_UPG_opt_M(u), UPG_opt_M(:,u)] = M_cal(N1, User_pre_grouping(:,:,u), K,...
+    [sum_UPG_opt_M(u), UPG_opt_M] = M_cal(N1, User_pre_grouping(:,:,u), K,...
                                         eplsion1R,eplsion2R,rho(u),N,eta);
     
     tic;
     %% Hill Climbing Pairing
     % RP and calculate blocklength as current optimum blocklength
     [sum_HCP_opt_M(u), HCP_opt_M(:,u)] = M_cal(N1, RP_user_pairing(:,:,u), K,...
-                                        eplsion1R,eplsion2R,rho(u),N,eta);
+                                        eplsion1R,eplsion2R,rho(u),eta,lamda);
     
-    cur_combinition = RP_user_pairing(:,:,u); 
+    cur_combinition = RP_user_pairing(:,:,u);
     while 1
         % find neighbor
-        [neighbor_1, neighbor_2] = neighbor_finder(cur_combinition, K);
-        [sum_nei1_opt_M, nei1_opt_M(:,u)] = M_cal(N1, neighbor_1, K,...
-                                        eplsion1R,eplsion2R,rho(u),N,eta);
-        [sum_nei2_opt_M, nei2_opt_M(:,u)] = M_cal(N1, neighbor_2, K,...
-                                        eplsion1R,eplsion2R,rho(u),N,eta);
+        [neighbor_1, neighbor_2, diff_idx] = neighbor_finder(cur_combinition, K);
+        % calculate sum of non-changing pair
+        tmp_sum = sum_HCP_opt_M(u) - HCP_opt_M(diff_idx(1),u) - HCP_opt_M(diff_idx(2),u);
         
+        % calculate sum of changing pair
+        [sum_nei1_opt_M, nei1_opt_M] = M_cal(N1, neighbor_1, 2,...
+                                        eplsion1R,eplsion2R,rho(u),eta,lamda);
+        sum_nei1_opt_M = tmp_sum + sum_nei1_opt_M;
+
+
+        [sum_nei2_opt_M, nei2_opt_M] = M_cal(N1, neighbor_2, 2,...
+                                        eplsion1R,eplsion2R,rho(u),eta,lamda);
+        sum_nei2_opt_M = tmp_sum + sum_nei2_opt_M;
+
+
         % Find the best neighbor
         if sum_nei1_opt_M < sum_nei2_opt_M
             sum_nei_opt_M = sum_nei1_opt_M;
-            nei_opt_M = nei1_opt_M(:,u);
+            nei_opt_M = nei1_opt_M;
             neighbor = neighbor_1;
         else
             sum_nei_opt_M = sum_nei2_opt_M;
-            nei_opt_M = nei2_opt_M(:,u);
+            nei_opt_M = nei2_opt_M;
             neighbor = neighbor_2;
         end
         
@@ -83,8 +106,10 @@ for u=1:length(Pt)
 
         if  sum_nei_opt_M <= sum_HCP_opt_M (u)
             sum_HCP_opt_M (u) = sum_nei_opt_M;
-            HCP_opt_M(:,u) = nei_opt_M;
-            cur_combinition = neighbor;
+            HCP_opt_M(diff_idx(1),u) = nei_opt_M(1);
+            HCP_opt_M(diff_idx(2),u) = nei_opt_M(2);
+            cur_combinition(diff_idx(1),:) = neighbor(1,:);
+            cur_combinition(diff_idx(2),:) = neighbor(2,:);
         else
             break;
         end
@@ -151,7 +176,7 @@ for u=1:length(Pt)
             
     end % End SAP
     Simulated_Anealing_Pairing(:,:,u) = cur_combinition;
-     [SAG_thred1_check(:,u), SAG_thred2_check(:,u)] = thred_checker(Simulated_Anealing_Pairing(:,:,u), K, eplsion1R, eplsion2R, rho(u), eta);
+%     [SAG_thred1_check(:,u), SAG_thred2_check(:,u)] = thred_checker(Simulated_Anealing_Pairing(:,:,u), K, eplsion1R, eplsion2R, rho(u), eta);
     toc;
     Timer(2,u) = toc;
 end
