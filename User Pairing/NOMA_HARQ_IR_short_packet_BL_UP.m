c4591f0ab74b62cd60a9430bf8db34527054b5a4
wj wang <eric79680227@gmail.com>
2020-12-23 15:21:59 +0800

    fix: function call, SAP intial parameter

--- User Pairing/NOMA_HARQ_IR_short_packet_BL_UP.m
+++ User Pairing/NOMA_HARQ_IR_short_packet_BL_UP.m
@@ -1,7 +1,7 @@
 clc; clear variables; close all;
 
 N = 1e6; % number of channel tap
-NNN = 2; % number of Monte Carlo
+NNN = 10; % number of Monte Carlo
 K = 5;  % number of cluster (number of user  = 2K)
 NN = 80; % number of information bit
 N1 = NN;
@@ -10,7 +10,9 @@ N2 = NN;
 eplsion1R = 10^-5;
 eplsion2R = 10^-4;
 
-Pt = -10:2:10;                    %Transmit Power in dBm
+s = 6;
+
+Pt = -10:2:10;               %Transmit Power in dBm
 pt = (10^-3).*db2pow(Pt);    %Transmit Power (linear scale)
 
 BW = 10^7;                  %System bandwidth
@@ -26,7 +28,13 @@ dis_thred1 = (eplsion2R/eplsion1R)^(1/eta);
 RP_user_pairing = zeros(K,2,length(Pt));
 User_pre_grouping = zeros(K,2,length(Pt));
 Simulated_Anealing_Pairing = zeros(K,2,length(Pt));
+Exhaustive_pairing = zeros(K,2,length(Pt));
 
+pair_idx_tmp = paircombs(2*K);
+pair_idx = 2*K+1-fliplr(pair_idx_tmp);
+
+
+sum_EP_opt_M_j = zeros(NNN,length(Pt));
 sum_RP_opt_M_j = zeros(NNN,length(Pt));
 sum_UPG_opt_M_j = zeros(NNN,length(Pt));
 sum_HCP_opt_M_j = zeros(NNN,length(Pt));
@@ -42,7 +50,7 @@ Timer_HCP = zeros(NNN,length(Pt));
 Timer_SAP = zeros(NNN,length(Pt));
 
 
-s = tic;
+x = tic;
 parfor u=1:length(Pt)
     h = (randn(1,N)+1i*randn(1,N));
     lamda = mean(abs(h).^2);
@@ -50,7 +58,25 @@ parfor u=1:length(Pt)
         % Generate user randomly
         user_distance = randi([10 330],1,2*K);
         user_distance = sort(user_distance);
+        exhaustive_pairing = reshape(user_distance(pair_idx)',K,2,length(pair_idx));
         
+        %% Exhaustive Paring (EP)
+        [tmp_sum_EP_M, tmp_EP_M] = M_cal(N1, exhaustive_pairing(:,:,1), K,...
+                                                eplsion1R,eplsion2R,rho(u),eta,lamda,s);
+        Exhaustive_pairing(:,:,u) = exhaustive_pairing(:,:,1);
+        for ll=2:length(exhaustive_pairing)
+            % Total blocklength for exhaustive paring
+            [sum_EP_M, EP_M] = M_cal(N1, exhaustive_pairing(:,:,ll), K,...
+                                                eplsion1R,eplsion2R,rho(u),eta,lamda,s);
+
+            if sum_EP_M < tmp_sum_EP_M
+                tmp_sum_EP_M = sum_EP_M;
+                tmp_EP_M = EP_M;
+                Exhaustive_pairing(:,:,u) = exhaustive_pairing(:,:,ll);
+            end       
+        end
+        sum_EP_opt_M_j(jj,u) = tmp_sum_EP_M;
+             
         a = tic;
         %% Random Paring (RP)
         RP_indices = randperm(2*K);
@@ -62,7 +88,7 @@ parfor u=1:length(Pt)
         RP_user_pairing(:,:,u) = tmp;
         % Total blocklength for random pairing
         [sum_RP_opt_M_j(jj,u), RP_opt_M] = M_cal(N1, RP_user_pairing(:,:,u), K,...
-                                        eplsion1R,eplsion2R,rho(u),eta,lamda); 
+                                        eplsion1R,eplsion2R,rho(u),eta,lamda,s); 
 
         Timer_RP(jj,u) = toc(a);                               
                          
@@ -71,7 +97,7 @@ parfor u=1:length(Pt)
         tmp = zeros(K,2);
         for ii=1:K
             tmp(ii,1) = user_distance(ii);
-            tmp(ii,2) = user_distance(K -1 + ii);
+            tmp(ii,2) = user_distance(K + ii);
 %             User_pre_grouping_thred_check(ii,u) = double((User_pre_grouping(ii,2,u) / User_pre_grouping(ii,1,u)) > dis_thred1);
         end
         
@@ -79,7 +105,7 @@ parfor u=1:length(Pt)
         
         % Total blocklength for User Pre-Grouping
         [sum_UPG_opt_M_j(jj,u)] = M_cal(N1, User_pre_grouping(:,:,u), K,...
-                                            eplsion1R,eplsion2R,rho(u),eta,lamda);                         
+                                            eplsion1R,eplsion2R,rho(u),eta,lamda,s);                         
         
         Timer_UPG(jj,u) = toc(b);
         
@@ -98,12 +124,12 @@ parfor u=1:length(Pt)
 
             % calculate sum of changing pair
             [sum_nei1_opt_M, nei1_opt_M] = M_cal(N1, neighbor_1, 2,...
-                                            eplsion1R,eplsion2R,rho(u),eta,lamda);
+                                            eplsion1R,eplsion2R,rho(u),eta,lamda,s);
             sum_nei1_opt_M = tmp_sum + sum_nei1_opt_M;
 
 
             [sum_nei2_opt_M, nei2_opt_M] = M_cal(N1, neighbor_2, 2,...
-                                            eplsion1R,eplsion2R,rho(u),eta,lamda);
+                                            eplsion1R,eplsion2R,rho(u),eta,lamda,s);
             sum_nei2_opt_M = tmp_sum + sum_nei2_opt_M;
 
 
@@ -137,9 +163,9 @@ parfor u=1:length(Pt)
         d = tic;
         %% Simulated Annealing Pairing
         % Initialization
-        Temperature = 20;
-        Temperature_min = 0.1;
-        annealing_factor = 0.7; 
+        Temperature = 50;
+        Temperature_min = 0.01;
+        annealing_factor = 0.9; 
         Time_budget = 50;
         cur_time = 0;
         % RP and calculate blocklength as current optimum blocklength
@@ -169,7 +195,7 @@ parfor u=1:length(Pt)
 
             % calculate sum of changing pair
             [sum_nei_opt_M, nei_opt_M] = M_cal(N1, neighbor, 2,...
-                                            eplsion1R,eplsion2R,rho(u),eta,lamda);
+                                            eplsion1R,eplsion2R,rho(u),eta,lamda,s);
             sum_nei_opt_M = tmp_sum + sum_nei_opt_M;
 
             % Find the solution for this iteration
@@ -206,25 +232,28 @@ parfor u=1:length(Pt)
     end
 
 end
+
+sum_EP_opt_M = mean(sum_EP_opt_M_j);
 sum_RP_opt_M = mean(sum_RP_opt_M_j);
 sum_UPG_opt_M = mean(sum_UPG_opt_M_j);
 sum_HCP_opt_M = mean(sum_HCP_opt_M_j);
 sum_SAP_opt_M = mean(sum_SAP_opt_M_j);
 
-t = toc(s)
+t = toc(x)
 
 
 figure (1)
 
 plot(Pt, sum_RP_opt_M,'b');
 hold on; grid on;
-plot(Pt, sum_UPG_opt_M,'g');
+plot(Pt, sum_UPG_opt_M,'-.cs');
 plot(Pt, sum_HCP_opt_M,'r');
 plot(Pt, sum_SAP_opt_M,'Color',[1 0.5 0]);
+plot(Pt, sum_EP_opt_M, 'mo');
 
 
 ylabel('blocklength');
-legend('Random Pairing','User Pre-Grouping','Hill Climbing Based Pairing','Simulated Anealing Based Pairing');
+legend('Random Pairing','User Pre-Grouping','Hill Climbing Based Pairing','Simulated Anealing Based Pairing', 'Exhaustive Paring');
 
 name_str = ['UP_test_' num2str(NNN) 'times.png'];
 saveas(gcf,name_str);
--- User Pairing/NOMA_HARQ_IR_short_packet_UP_CDF.m
+++ User Pairing/NOMA_HARQ_IR_short_packet_UP_CDF.m
@@ -7,6 +7,7 @@ N2 = NN;
 
 eplsion1R = 10^-5;
 eplsion2R = 10^-4;
+s = 6;
 
 Pt = 0;                    %Transmit Power in dBm
 pt = (10^-3)*db2pow(Pt);    %Transmit Power (linear scale)
@@ -21,6 +22,9 @@ RHO = pow2db(rho);
 eta = 4;
 
 for u=1:length(ncluster)
+    h = (randn(1,N)+1i*randn(1,N));
+    lamda = mean(abs(h).^2);
+    
     K = ncluster(u);
     if u == 1
         user_distance = randi([10 330],1,2*K);
@@ -42,23 +46,25 @@ for u=1:length(ncluster)
     %% User Pre-Grouping
     for ii=1:K
         User_pre_grouping(ii,1) = user_distance(ii);
-        User_pre_grouping(ii,2) = user_distance(K -1 + ii);
+        User_pre_grouping(ii,2) = user_distance(K + ii);
     end
     
-    % Total blocklength for User Pre-Grouping
+    % Total blocklength for User Pre-Grouping                                    
     [sum_UPG_opt_M(u)] = M_cal(N1, User_pre_grouping(:,:), K,...
-                                        eplsion1R,eplsion2R,rho,N,eta);
+                                    eplsion1R,eplsion2R,rho,eta,lamda,s);
     
+
     %% Simulated Annealing Pairing
     % Initialization
-    Temperature = 20;
-    Temperature_min = 0.1;
-    annealing_factor = 0.7; 
-    Time_budget = 50;
+    Temperature = 50;
+    Temperature_min = 0.01;
+    annealing_factor = 0.9; 
+    Time_budget = 100;
     cur_time = 0;
     % RP and calculate blocklength as current optimum blocklength
-    [sum_SAP_opt_M(u)] = M_cal(N1, RP_user_pairing(:,:), K,...
-                                        eplsion1R,eplsion2R,rho,N,eta);
+    SAP_opt_M = zeros(K,length(ncluster));
+    [sum_SAP_opt_M(u), SAP_opt_M(:,u)] = M_cal(N1, RP_user_pairing(:,:), K,...
+                                        eplsion1R,eplsion2R,rho,eta,lamda,s);
                                     
     cur_combinition = RP_user_pairing(:,:); 
     while 1
@@ -69,22 +75,30 @@ for u=1:length(ncluster)
             break;
         end
         % Find neighbor
-        [neighbor_1, neighbor_2] = neighbor_finder(cur_combinition, K);
+        [neighbor_1, neighbor_2, diff_idx] = neighbor_finder(cur_combinition, K);
         % Choose neighbor randomly
         if randi(2) == 1
             neighbor = neighbor_1;
         else
             neighbor = neighbor_2;
         end
-        % Calculate blocklength for neighbor
-        [sum_nei_opt_M] = M_cal(N1, neighbor, K,...
-                                        eplsion1R,eplsion2R,rho,N,eta);
+
+        % calculate sum of non-changing pair
+        tmp_sum = sum_SAP_opt_M(u) - SAP_opt_M(diff_idx(1),u) - SAP_opt_M(diff_idx(2),u);
+        
+        % calculate sum of changing pair
+        [sum_nei_opt_M, nei_opt_M] = M_cal(N1, neighbor, 2,...
+                                        eplsion1R,eplsion2R,rho,eta,lamda,s);
+        sum_nei_opt_M = tmp_sum + sum_nei_opt_M;
         
         % Find the solution for this iteration
         % Better solution
-        if  sum_nei_opt_M <= sum_SAP_opt_M (u)
+        if  sum_nei_opt_M < sum_SAP_opt_M (u)
             sum_SAP_opt_M (u) = sum_nei_opt_M;
-            cur_combinition = neighbor;
+            SAP_opt_M(diff_idx(1),u) = nei_opt_M(1);
+            SAP_opt_M(diff_idx(2),u) = nei_opt_M(2);
+            cur_combinition(diff_idx(1),:) = neighbor(1,:);
+            cur_combinition(diff_idx(2),:) = neighbor(2,:);
         % Worse solution
         else
             delta_E = sum_SAP_opt_M (u) - sum_nei_opt_M;
@@ -92,7 +106,10 @@ for u=1:length(ncluster)
             % Accept worse solution
             if exp(delta_E/Temperature) >= rn
                 sum_SAP_opt_M (u) = sum_nei_opt_M;
-                cur_combinition = neighbor;
+                SAP_opt_M(diff_idx(1),u) = nei_opt_M(1);
+                SAP_opt_M(diff_idx(2),u) = nei_opt_M(2);
+                cur_combinition(diff_idx(1),:) = neighbor(1,:);
+                cur_combinition(diff_idx(2),:) = neighbor(2,:);
             end           
         end
         % Annealing
@@ -102,8 +119,8 @@ for u=1:length(ncluster)
         end
             
     end % End SAP
-    
-    
+%     Simulated_Anealing_Pairing(:,:,u) = cur_combinition;
+
 end
 
 figure (1)
